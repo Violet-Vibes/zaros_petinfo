66b61e7ab639d4c3b9ff66a321ed34ffd4630967
Violet Vibes <qsato@protonmail.com>
2022-06-21 16:58:43 +0200

    Add files via upload

---
+++ PetInfoPlugin.java
@@ -0,0 +1,436 @@
+/* Copyright (c) 2020 by micro
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Portions of the code are based off of the "Implings" RuneLite plugin.
+ * The "Implings" is:
+ * Copyright (c) 2017, Robin <robin.weymans@gmail.com>
+ * All rights reserved.
+ *
+ * Portions of the code are based off of the "Menu Entry Swapper" RuneLite plugin.
+ * The "Menu Entry Swapper" is:
+ * Copyright (c) 2018, Adam <Adam@sigterm.info>
+ * Copyright (c) 2018, Kamiel
+ * Copyright (c) 2019, Rami <https://github.com/Rami-J>
+ * All rights reserved.
+ */
+
+package net.runelite.client.plugins.petinfo;
+
+import com.google.gson.Gson;
+import com.google.inject.Provides;
+import net.runelite.client.plugins.petinfo.dataretrieval.PetDataFetcher;
+import net.runelite.client.plugins.petinfo.dataretrieval.Pet;
+import net.runelite.client.plugins.petinfo.dataretrieval.PetGroup;
+import net.runelite.client.plugins.petinfo.dataretrieval.PetInfo;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.*;
+import net.runelite.api.Point;
+import net.runelite.api.events.*;
+import net.runelite.client.chat.ChatMessageBuilder;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.overlay.OverlayManager;
+import okhttp3.OkHttpClient;
+
+import javax.inject.Inject;
+import java.awt.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Slf4j
+@PluginDescriptor(
+		name = "Pet Info",
+		description = "Highlights other players pets,shows the pets name and gives info about the pets.",
+		tags = {"pet"}
+)
+public class PetInfoPlugin extends Plugin
+{
+	// Use these two arrays instead of cachedNPC index to guard against despawn race-condition
+	@Getter(AccessLevel.PACKAGE)
+	private final List<NPC> pets = new ArrayList<>();	// Used to keep track of the current pets in the game
+
+	@Getter(AccessLevel.PACKAGE)
+	private final Color defaultYellow = new Color(0xffff00);
+
+	@Inject
+	private Client client;
+
+	@Inject
+	private OverlayManager overlayManager;
+
+	@Inject
+	private PetsOverlay overlay;
+
+	@Inject
+	private PetsConfig config;
+
+	@Inject
+	private OkHttpClient okHttpClient;
+
+	@Inject
+	private Gson gson;
+
+	private PetInfo petInfo;
+
+	private final String MENU_OPTION = "Info";
+
+	@Provides
+	PetsConfig getConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(PetsConfig.class);
+	}
+
+	@Override
+	protected void startUp()
+	{
+		overlayManager.add(overlay);
+
+		try
+		{
+			petInfo = new PetInfo(new PetDataFetcher(okHttpClient, gson), config.getRemoteData());
+		}
+		catch (IOException e)
+		{
+			log.error("[Pet-Info]\tCould not load pet data... Shutting plugin down...");
+			shutDown();
+		}
+	}
+
+	@Override
+	protected void shutDown()
+	{
+		pets.clear();
+		overlayManager.remove(overlay);
+	}
+
+	@Subscribe
+	public void onNpcSpawned(NpcSpawned npcSpawned)
+	{
+		NPC npc = npcSpawned.getNpc();
+		Pet pet = petInfo.findPet(npc.getId());
+
+		if (pet != null)
+		{
+			pets.add(npc);
+		}
+	}
+
+	@Subscribe
+	public void onNpcChanged(NpcChanged npcCompositionChanged)	// Do pet's compositions ever change? If they do we need to handle if they ever stop being pets,
+	{															// if they don't we don't need this at all. I think this may have cause the highlight with no pet issue.
+		NPC npc = npcCompositionChanged.getNpc();
+		Pet pet = petInfo.findPet(npc.getId());
+
+		if (pet != null)
+		{
+			if (!pets.contains(npc))
+			{
+				pets.add(npc);
+			}
+		}
+	}
+
+	@Subscribe
+	public void onGameStateChanged(GameStateChanged event)
+	{
+		if (event.getGameState() == GameState.LOGIN_SCREEN || event.getGameState() == GameState.HOPPING)
+		{
+			pets.clear();
+		}
+	}
+
+	@Subscribe
+	public void onNpcDespawned(NpcDespawned npcDespawned)
+	{
+		if (pets.isEmpty())
+		{
+			return;
+		}
+
+		NPC npc = npcDespawned.getNpc();
+		pets.remove(npc);
+	}
+
+	/**
+	 * Add the menus for the pets, must be done each tick or else they would disappear on the next tick.
+	 * This is not done in onMenuOpened because the entries are visibly added after the fact.
+	 */
+	@Subscribe
+	public void onClientTick(ClientTick clientTick)
+	{
+		// Ensure we are going to be showing the menus, that a menu can be show at all, or an existing menu is not already open
+		if (!config.showMenu() || client.getGameState() != GameState.LOGGED_IN || client.isMenuOpen())
+		{
+			return;
+		}
+
+		addMenus();
+	}
+
+	/**
+	 * Prints to chat the appropriate text for the given menu
+	 */
+	public void onMenuOptionClicked(MenuEntry event)
+	{
+		// If the player did not click on an INFO menu entry (hopefully should lever happen with new api)
+		if (event.getType() != MenuAction.RUNELITE || !(event.getOption().startsWith("Info")))
+		{
+			log.error("[Pet-Info]\tSomehow got an incorrect menu entry?", event);
+			return;
+		}
+
+		// We get the info text based off of the pet's NPCid
+		client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "The " + event.getTarget()
+				+ " " + petInfo.getInfo(event.getIdentifier()), "");
+	}
+
+	/**
+	 * Finds the {@link net.runelite.client.plugins.petinfo.PetsConfig.PetMode} of a given NPC.
+	 */
+	PetsConfig.PetMode showNpc(NPC npc)
+	{
+		Pet pet = petInfo.findPet(npc.getId());
+		if (pet == null)
+		{
+			return PetsConfig.PetMode.OFF;
+		}
+
+		switch (pet.petGroup)
+		{
+			case BOSS:
+				return config.showBoss();
+			case SKILLING:
+				return config.showSkilling();
+			case TOY:
+				return config.showToy();
+			case OTHER:
+				return config.showOther();
+			default:
+				return PetsConfig.PetMode.OFF;
+		}
+	}
+
+	/**
+	 * Finds the color a given NPCs {@link PetGroup} should be displayed in
+	 * @param npc the npc to get the color of
+	 * @return the color of the npc based on the pet group
+	 */
+	Color npcToColor(NPC npc)
+	{
+		Pet pet = petInfo.findPet(npc.getId());
+		if (pet == null)
+		{
+			return null;
+		}
+
+		switch (pet.petGroup)
+		{
+			case BOSS:
+				return config.getBossColor();
+			case SKILLING:
+				return config.getSkillingColor();
+			case TOY:
+				return config.getToyColor();
+			case OTHER:
+				return config.getOtherColor();
+			default:
+				return null;
+		}
+	}
+
+	/**
+	 * Finds which pets are under the users cursor.
+	 * This consults the {@link #pets} array.
+	 */
+	private List<NPC> getPetsUnderCursor()
+	{
+		Point mouseCanvasPosition = client.getMouseCanvasPosition();
+		if (!mouseIsBlocked())
+		{
+			List<NPC> list = new ArrayList<>();
+			for (NPC p : pets)
+			{
+				if (isClickable(p, mouseCanvasPosition))
+				{
+					list.add(p);
+				}
+			}
+			return list;
+		}
+
+		return Collections.emptyList();
+	}
+
+	/**
+	 * Hacky way to see if the mouse is blocked;
+	 * check to see if you can walk to the mouse position.
+	 * This is instead of building a tree of widgets to search
+	 */
+	private boolean mouseIsBlocked()
+	{
+		List<MenuEntry> list = new ArrayList<>();
+		for (MenuEntry menuEntry : client.getMenuEntries())
+		{
+			if (menuEntry.getType() == MenuAction.WALK)
+			{
+				list.add(menuEntry);
+			}
+		}
+		return list.isEmpty();
+	}
+
+	private boolean isClickable(NPC npc, Point mouseCanvasPosition)
+	{
+		Shape npcHull = npc.getConvexHull();	// Gets a Shape representing an outline of the npc
+
+		if (npcHull != null)
+		{
+			// Determine if the cursor is inside of the outline of the pet
+			return npcHull.contains(mouseCanvasPosition.getX(), mouseCanvasPosition.getY());
+		}
+
+		return false;
+	}
+
+	/**
+	 * Adds the menus for the the pets that are under the cursor at the time of the call.
+	 */
+	private void addMenus()
+	{
+		List<NPC> petsUnderCursor = getPetsUnderCursor();
+		if (!petsUnderCursor.isEmpty())
+		{
+			for (NPC pet : petsUnderCursor)
+			{
+				addPetInfoMenu(pet);
+			}
+		}
+	}
+
+	private void addPetInfoMenu(NPC pet)
+	{
+		String option = MENU_OPTION;
+		if(pet.getInteracting() != null)
+		{
+			option += " " + colorOwnerName(pet.getInteracting()) + "'s";
+		}
+
+		client.createMenuEntry(-1)
+				.setOption(option)
+				.setTarget(colorPetName(pet))
+				.setType(MenuAction.RUNELITE)
+				.setIdentifier(pet.getId())
+				.onClick(this::onMenuOptionClicked);
+	}
+
+	private String colorOwnerName(Actor owner)
+	{
+		Color ownerColor;
+
+		switch (config.petOwnerColor())
+		{
+			case YELLOW:
+				ownerColor = defaultYellow;
+				break;
+
+			case COMBAT:
+				Actor player = client.getLocalPlayer();
+
+				if (player == null) {
+					ownerColor = defaultYellow;
+					log.error("[Pet-Info]\tLocal player was null. Defaulting to yellow menu color.");
+				}
+				else {
+					ownerColor = getNameColorFromCombatLevels(
+						player.getCombatLevel(),
+						owner.getCombatLevel()
+					);
+				}
+				break;
+
+			default:
+				return  owner.getName();
+		}
+
+		return colorChatString(ownerColor, owner.getName());
+	}
+
+	private String colorPetName(NPC pet)
+	{
+		String petName = pet.getName();
+		switch (config.petInfoColor())
+		{
+			case HIGHLIGHT:
+			{
+				return colorChatString(npcToColor(pet), petName);
+			}
+			case YELLOW:
+			default:
+			{
+				return colorChatString(defaultYellow, petName);
+			}
+		}
+
+	}
+
+	private String colorChatString(Color color, String name)
+	{
+		ChatMessageBuilder petNameColored = new ChatMessageBuilder().append(color, name);
+		return petNameColored.build();
+	}
+
+	private Color getNameColorFromCombatLevels(int localPlayerLevel, int otherPlayerLevel)
+	{
+		int delta = localPlayerLevel - otherPlayerLevel;
+
+		if (delta < -9) {
+			return new Color(0xff0000);
+		}
+		if (delta < -6) {
+			return new Color(0xff3000);
+		}
+		if (delta < -3) {
+			return new Color(0xff7000);
+		}
+		if (delta < 0) {
+			return new Color(0xffb000);
+		}
+		if (delta > 9) {
+			return new Color(0xff00);
+		}
+		if (delta > 6) {
+			return new Color(0x40ff00);
+		}
+		if (delta > 3) {
+			return new Color(0x80ff00);
+		}
+		if (delta > 0) {
+			return new Color(0xc0ff00);
+		}
+		return new Color(0xffff00);
+	}
+}
---
+++ PetsConfig.java
@@ -0,0 +1,224 @@
+/* Copyright (c) 2020 by micro
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Portions of the code are based off of the "Implings" RuneLite plugin.
+ * The "Implings" is:
+ * Copyright (c) 2017, Robin <robin.weymans@gmail.com>
+ * All rights reserved.
+ */
+
+package net.runelite.client.plugins.petinfo;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
+
+import java.awt.*;
+
+@ConfigGroup("pets")
+public interface PetsConfig extends Config
+{
+	@ConfigSection(
+			name = "Pet group highlight settings",
+			description = "Choose how and what color pet groups (i.e. bossing, skilling) should be highlighted.",
+			position = 99
+	)
+	String highlightSection = "section";
+
+	enum PetMode
+	{
+		OFF,
+		HIGHLIGHT,
+		NAME_ONLY
+	}
+
+	enum PetInfoColor
+	{
+		HIGHLIGHT,
+		YELLOW
+	}
+
+	enum PetOwnerColor
+	{
+		WHITE,
+		YELLOW,
+		COMBAT
+	}
+
+	enum HighlightMode
+	{
+		OFF,
+		ALL,
+		OWN
+	}
+
+	@ConfigItem(
+			position = 1,
+			keyName = "petInfo",
+			name = "Right click menu",
+			description = "Show pet Info and Owner option on right click"
+	)
+	default boolean showMenu()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+			position = 2,
+			keyName = "getRemoteData",
+			name = "Get up-to-date list of pets",
+			description = "Use the updated list of pets from the github. If off, and no local backup found, the plugin will not work. A local backup will be created on first download of an updated version."
+	)
+	default boolean getRemoteData() { return true; }
+
+	@ConfigItem(
+			position = 3,
+			keyName = "togglePetInfoColor",
+			name = "Pet name color",
+			description = "Choose between the RuneScape default yellow and the custom highlight colors for the pets name"
+	)
+	default PetInfoColor petInfoColor() { return PetInfoColor.YELLOW; }
+
+	@ConfigItem(
+			position = 4,
+			keyName = "togglePetOwnerColor",
+			name = "Pet's owner name color",
+			description = "Choose between the RuneScape default yellow, white, or the combat level differential"
+	)
+	default PetOwnerColor petOwnerColor() { return PetOwnerColor.WHITE; }
+
+	@ConfigItem(
+			position = 5,
+			keyName = "toggleHighlight",
+			name = "Highlight toggle",
+			description = "Select if no, all, or only your own pets are highlighted"
+	)
+	default HighlightMode highlight() { return HighlightMode.OFF; }
+
+	@ConfigItem(
+			position = 6,
+			keyName = "showBoss",
+			name = "Highlight Bossing Pets",
+			description = "Toggles highlighting for bossing pets",
+			section = highlightSection
+	)
+	default PetMode showBoss()
+	{
+		return PetMode.HIGHLIGHT;
+	}
+
+	@ConfigItem(
+			position = 7,
+			keyName = "bossColor",
+			name = "Boss Pet color",
+			description = "Highlight color for boss pets",
+			section = highlightSection
+	)
+	default Color getBossColor()
+	{
+		return new Color(193, 18, 18);
+	}
+
+	@ConfigItem(
+			position = 8,
+			keyName = "showSkilling",
+			name = "Highlight Skilling Pets",
+			description = "Toggles highlighting for skilling pets",
+			section = highlightSection
+	)
+	default PetMode showSkilling()
+	{
+		return PetMode.HIGHLIGHT;
+	}
+
+	@ConfigItem(
+			position = 9,
+			keyName = "skillingColor",
+			name = "Skilling Pet color",
+			description = "Highlight color for skilling pets",
+			section = highlightSection
+	)
+	default Color getSkillingColor()
+	{
+		return new Color(106, 232, 38);
+	}
+
+	@ConfigItem(
+			position = 10,
+			keyName = "showToy",
+			name = "Highlight Toys",
+			description = "Toggles highlighting for clockwork toys",
+			section = highlightSection
+	)
+	default PetMode showToy()
+	{
+		return PetMode.OFF;
+	}
+
+	@ConfigItem(
+			position = 11,
+			keyName = "toyColor",
+			name = "Toy color",
+			description = "Highlight color for clockwork toys",
+			section = highlightSection
+	)
+	default Color getToyColor()
+	{
+		return new Color(139, 120, 69);
+	}
+
+	@ConfigItem(
+			position = 12,
+			keyName = "showOther",
+			name = "Show Other Pets",
+			description = "Toggles highlighting for other pets (like cats)",
+			section = highlightSection
+	)
+	default PetMode showOther()
+	{
+		return PetMode.NAME_ONLY;
+	}
+
+	@ConfigItem(
+			position = 13,
+			keyName = "otherColor",
+			name = "Other Pet color",
+			description = "Highlight color for other pets",
+			section = highlightSection
+	)
+	default Color getOtherColor()
+	{
+		return new Color(18, 47, 193);
+	}
+
+	@ConfigItem(
+			position = 14,
+			keyName = "showNpcId",
+			name = "Show NPC ID",
+			description = "Show the pets NPC id next to its overhead name",
+			section = highlightSection
+	)
+	default boolean getShowNpcId()
+	{
+		return false;
+	}
+}
